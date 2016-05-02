7d07d1d6dded91dd7ce6bb4b5c66997f22ed26c4
David Eriksson <david@2good.nu>
2016-05-02 21:44:11 +0200

    Run SHUTDOWN SAVE on SIGTERM and SIGINT

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
 if [ "$1" = 'redis-server' ]; then
-     chown -R redis .
-         exec gosu redis "$@" --module-add /usr/lib/geo.so
-     fi
+	chown -R redis .
+	gosu redis "$@" --module-add /usr/lib/geo.so &
+	PID=$!
+	trap "redis-cli SHUTDOWN SAVE" TERM INT
+	wait $PID
+	trap - TERM INT
+	wait $PID
+fi
 exec "$@"
